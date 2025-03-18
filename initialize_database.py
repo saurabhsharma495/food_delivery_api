8a3d37f292d7929546fa0086ffaef065b68c275a
Saurabh <saurabhksharma500@gmail.com>
2025-03-18 10:33:58 +0530

    jwt access and refresh token has been implemented

--- initialize_database.py
+++ initialize_database.py
@@ -1,5 +1,26 @@
-from db_config import engine, Base
-from models import User, Order
-
+from db_config import Base
+from sqlalchemy.exc import OperationalError
+from fastapi import HTTPException
+from sqlalchemy import inspect
+from db_config import engine
 
 Base.metadata.create_all(bind=engine)
+
+
+def check_and_create_table(db_engine):
+    try:
+        tables_list = list(Base.metadata.tables.keys())
+        inspector = inspect(db_engine)  # Use inspect() to inspect the engine
+
+        # Check if each table exists
+        for table_name in tables_list:
+            if table_name not in inspector.get_table_names():
+                print(f"Table '{table_name}' does not exist, creating it...")
+                # Create the table if it doesn't exist
+                Base.metadata.create_all(bind=db_engine)
+            else:
+                print(f"Table '{table_name}' already exists.")
+
+    except OperationalError as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
+
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from fastapi_jwt_auth import AuthJWT
+from initialize_database import check_and_create_table
+from schemas import Settings
 from auth_routes import auth_router
 from order_routes import order_router
+from db_config import engine, DATABASE_URL
 
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    check_and_create_table(engine)
+
+
+@app.on_event("shutdown")
+def shutdown():
+    print("Shutting down the FastAPI server...")
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+
 app.include_router(auth_router)
 app.include_router(order_router)
-
